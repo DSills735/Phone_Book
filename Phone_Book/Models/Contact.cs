58c9eb988383b7cc2d4a9c534f7d8321b8797f5b
DSills735 <David.sills100@gmail.com>
2026-03-12 20:14:15 -0500

    Add project files.

---
+++ Phone_Book/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Phone_Book.Models
+{
+    internal class Contact
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+
+    }
+}
---
+++ Phone_Book/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Phone_Book
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //connnection LocalDB/PhoneBook
+        }
+    }
+
+}
